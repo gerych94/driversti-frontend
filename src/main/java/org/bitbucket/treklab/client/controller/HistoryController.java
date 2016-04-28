a30fe6165de122c968d367bdf5b8d60d1c3d4697
driversti <driversti@gmail.com>
2016-04-28 16:39:06 +0300

    Added templates for PlacesController and HistoryController

---
+++ src/main/java/org/bitbucket/treklab/client/controller/HistoryController.java
@@ -0,0 +1,6 @@
+package org.bitbucket.treklab.client.controller;
+
+public class HistoryController {
+
+    private static final String className = HistoryController.class.getSimpleName();
+}
---
+++ src/main/java/org/bitbucket/treklab/client/controller/PlacesController.java
@@ -0,0 +1,6 @@
+package org.bitbucket.treklab.client.controller;
+
+public class PlacesController {
+
+    private static final String className = PlacesController.class.getSimpleName();
+}
