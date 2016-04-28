67c6d00d725ea5b61e30b43a1e5d682b7a61ca77
driversti <driversti@gmail.com>
2016-04-28 17:07:20 +0300

    Tab "Info" is done

--- src/main/java/org/bitbucket/treklab/client/view/DevicePropertiesDialog.java
+++ src/main/java/org/bitbucket/treklab/client/view/DevicePropertiesDialog.java
@@ -1,6 +1,12 @@
 package org.bitbucket.treklab.client.view;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsonUtils;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -8,8 +14,25 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.widget.core.client.Window;
+import com.sencha.gxt.widget.core.client.box.AlertMessageBox;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
+import com.sencha.gxt.widget.core.client.grid.ColumnModel;
+import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.grid.GroupingView;
+import com.sencha.gxt.widget.core.client.info.Info;
+import org.bitbucket.treklab.client.communication.BaseRequestCallback;
+import org.bitbucket.treklab.client.communication.PositionData;
 import org.bitbucket.treklab.client.model.Device;
+import org.bitbucket.treklab.client.model.InfoRow;
+import org.bitbucket.treklab.client.model.InfoRowProperties;
+import org.bitbucket.treklab.client.model.Position;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class DevicePropertiesDialog extends Composite {
     interface DevicePropertiesDialogUiBinder extends UiBinder<Widget, DevicePropertiesDialog> {
@@ -24,8 +47,100 @@ public class DevicePropertiesDialog extends Composite {
         return window;
     }
 
+
+    @UiField
+    Grid<InfoRow> infoRowGrid;
+    @UiField(provided = true)
+    ListStore<InfoRow> infoRowStore;
+    @UiField(provided = true)
+    ColumnModel<InfoRow> infoRowCM;
+
+    @UiField
+    GroupingView<InfoRow> infoRowView;
+
+    private static final InfoRowProperties prop = GWT.create(InfoRowProperties.class);
+
     public DevicePropertiesDialog(Device selectedItem, ListStore<Device> deviceStore) {
+        this.infoRowStore = new ListStore<>(prop.key());
+
+        ColumnConfig<InfoRow, String> colName = new ColumnConfig<>(prop.name(), 150, "Параметр");
+        colName.setResizable(true);
+        ColumnConfig<InfoRow, String> colValue = new ColumnConfig<>(prop.value(), 100, "Значение");
+        colValue.setResizable(true);
+
+        List<ColumnConfig<InfoRow, ?>> configList = new ArrayList<>();
+        configList.add(colName);
+        configList.add(colValue);
+
+        this.infoRowCM = new ColumnModel<>(configList);
+        fillGrid(selectedItem);
+
         uiBinder.createAndBindUi(this);
+
+        this.infoRowView.setAutoExpandColumn(colValue);
+        this.infoRowView.setStripeRows(true);
+    }
+
+    private void fillGrid(final Device device) {
+        try {
+            new PositionData().getPositions(new BaseRequestCallback() {
+                @Override
+                public void onResponseReceived(Request request, Response response) {
+                    if (200 == response.getStatusCode()) {
+                        JsArray<Position> positions = JsonUtils.safeEval(response.getText());
+                        for (int i = 0; i < positions.length(); i++) {
+                            if (positions.get(i).getDeviceId() == device.getId()) {
+                                if (positions.get(i).getAddress() != null && !positions.get(i).getAddress().equals("")) {
+                                    infoRowStore.add(new InfoRow(0, "Адрес", positions.get(i).getAddress()));
+                                } else {
+                                    infoRowStore.add(new InfoRow(0, "Адрес", ""));
+                                }
+                                if (positions.get(i).getDeviceTime() != null && !positions.get(i).getDeviceTime().equals("")) {
+                                    Date tmp = new Date(positions.get(i).getDeviceTime());
+                                    DateTimeFormat fmt = DateTimeFormat.getFormat("yyyy-MM-dd HH:mm:ss");
+                                    infoRowStore.add(new InfoRow(1, "Время устройства", fmt.format(tmp)));
+                                }
+                                if (positions.get(i).getAltitude() >= 0) {
+                                    infoRowStore.add(new InfoRow(2, "Высота", positions.get(i).getAltitude() + " м"));
+                                } else {
+                                    infoRowStore.add(new InfoRow(2, "Высота", 0 + " м"));
+                                }
+                                infoRowStore.add(new InfoRow(3, "Долгота", String.valueOf(positions.get(i).getLongitude())));
+                                infoRowStore.add(new InfoRow(4, "Широта", String.valueOf(positions.get(i).getLatitude())));
+                                double speed = new BigDecimal(positions.get(i).getSpeed()).setScale(1, RoundingMode.UP).doubleValue();
+                                infoRowStore.add(new InfoRow(5, "Скорость", speed + " км/ч"));
+                                if (speed > 50) {
+                                    Info.display("Overspeed!", "Allowed speed: 50 km/h. Current speed: " + speed + " km/h");
+                                }
+                                infoRowStore.add(new InfoRow(6, "Курс", positions.get(i).getCourse() + "°"));
+                                if (positions.get(i).getProtocol() != null && !positions.get(i).getProtocol().equals("")) {
+                                    infoRowStore.add(new InfoRow(7, "Протокол", positions.get(i).getProtocol()));
+                                }
+                                if (positions.get(i).getAttributes().getIp() != null && !positions.get(i).getAttributes().getIp().equals("")) {
+                                    infoRowStore.add(new InfoRow(8, "IP", positions.get(i).getAttributes().getIp()));
+                                }
+                                if (!String.valueOf(positions.get(i).getAttributes().getPriority()).equals("undefined") /*&&
+                                        positions.get(i).getAttributes().getPriority() != 0*/) {
+                                    infoRowStore.add(new InfoRow(9, "Приоритет", String.valueOf(positions.get(i).getAttributes().getPriority())));
+                                }
+                                if (!String.valueOf(positions.get(i).getAttributes().getSat()).equals("undefined")) {
+                                    infoRowStore.add(new InfoRow(10, "Сат", String.valueOf(positions.get(i).getAttributes().getSat())));
+                                }
+                                if (!String.valueOf(positions.get(i).getAttributes().getEvent()).equals("undefined") /*&&
+                                        positions.get(i).getAttributes().getEvent() != 0*/) {
+                                    infoRowStore.add(new InfoRow(11, "Событие", String.valueOf(positions.get(i).getAttributes().getEvent())));
+                                }
+                                infoRowStore.add(new InfoRow(12, "Параметры", "-"));
+                            }
+                        }
+                    } else {
+                        new AlertMessageBox("Error", "Can't get position from server").show();
+                    }
+                }
+            });
+        } catch (RequestException e) {
+            e.printStackTrace();
+        }
     }
 
     public void show() {
