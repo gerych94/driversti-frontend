6a8a4187533d10cb703d46c89a41e82f14027a07
driversti <driversti@gmail.com>
2016-05-20 18:00:45 +0300

    i18n of the App has been started

---
+++ src/main/java/org/bitbucket/treklab/client/i18n/Constants.java
@@ -0,0 +1,17 @@
+package org.bitbucket.treklab.client.i18n;
+
+/**
+ * Created by driversti on 19.05.2016.
+ */
+public interface Constants extends com.google.gwt.i18n.client.Constants {
+
+    /*String name();
+
+    String value();
+
+    String device();
+
+    String position();
+
+    String geofence();*/
+}
